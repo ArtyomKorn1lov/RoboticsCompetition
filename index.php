b7990dcbd1a0ad8d54cabaab5d678be012c32de1
ArtyomKorn1lov <85835332+ArtyomKorn1lov@users.noreply.github.com>
2025-01-31 23:02:51 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Главная страница сайта");
+?>
+
+<?php
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ local/templates/robot/header.php
@@ -0,0 +1,16 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+IncludeTemplateLangFile(__FILE__);
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php $APPLICATION->ShowHead();?>
+    <title><?php $APPLICATION->ShowTitle()?></title>
+</head>
+<body>
+<div id="panel"><?$APPLICATION->ShowPanel();?></div>
